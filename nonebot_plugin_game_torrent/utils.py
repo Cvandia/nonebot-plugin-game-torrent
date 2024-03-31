7f3b085da24fe5390ff0085ff2d602732058afa0
divandia <106718176+Cvandia@users.noreply.github.com>
2024-03-31 12:05:29 +0800

    Add .deepsource.toml, LICENCE, README.md, nonebot_plugin_game_torrent/__init__.py, nonebot_plugin_game_torrent/utils.py, and pyproject.toml files

---
+++ nonebot_plugin_game_torrent/utils.py
@@ -0,0 +1,16 @@
+from nonebot.matcher import Matcher
+from nonebot.rule import Rule
+from nonebot.typing import T_State
+from nonebot.adapters import Bot, Event
+from httpx import AsyncClient
+from typing import Optional, Union
+from fake_useragent import UserAgent
+
+class FetchTorrent:
+
+    
+
+
+
+async def event_matcher(bot, event):
+    Fetch = FetchTorrent()
